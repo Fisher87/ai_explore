26bd099539d34cad94fbdb48c0128ba2292b8705
yulh <yulh@tuya.com>
2019-12-22 21:39:41 +0800

    update

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
+AI_EXPLORE
+![](https://readthedocs.org/projects/pygorithm/badge/?version=latest) ![](https://img.shields.io/badge/python%20-%202.7-brightgreen.svg)
+========
+> 包含机器学习、深度学习基础知识. 推荐系统及nlp相关算法;
 
 ## 机器学习[ml_explore]
 
@@ -5,11 +9,45 @@
 
 #### deep learning
 
+---
+
 ## 自然语言处理[nlp_explore]
+### 基础模型实现[basic_model]
++ DNN
++ CNN
++ DNN
++ RNN/LSTM
++ Transformer
++ HighWay
++ modules: 基础模型常用函数模块
 
-
+### 常用nlp任务[tasks]
++ classify
++ ner
++ text_match:
+  + lexical:
+    + BM25
+    + LSI
+    + TFIDF
+  + semantic:
+    + DSSM
+    + ABCNN
+    + TPConvNet
+    + ESIM
+    + BIMPM
 
 ---
 
 ## 推荐系统[recommend_explore]
+### 常用召回策略[recall]
++ CF:
+  + item_based
+  + user_based
+  + graph
++ LMF
+  + ALS
+  + MF
++ DeepMatch:
+
+### 常用排序策略[rank]
 
--- nlp_explore/basic_model/modules.py
+++ nlp_explore/basic_model/modules.py
@@ -187,3 +187,19 @@ def ln(x, epsilon=1e-8, scope="ln"):
 
     return outputs
 
+# learning rate func.
+def noam_scheme(init_lr, global_step, warmup_steps=4000.):
+    '''
+    Noam scheme learning rate decay.
+    @param warmup_steps: scalar. During warmup_steps, learning rate increases 
+                                until it reaches init_lr.
+    e.g.
+    >>> import tensorflow as tf
+    >>> global_step = tf.train.get_or_create_global_step()
+    >>> lr = noam_scheme(0.0003, global_step, warmup_steps=4000)
+    >>> optimizer = tf.train.AdamOptimizer(lr)
+    >>> train_op = optimizer.minimize(loss, global_step=global_step)
+    '''
+
+    step = tf.cast(global_step + 1, dtype=tf.float32)
+    return init_lr * warmup_steps**0.5 * tf.minimum(step * warmup_steps ** -1.5, step ** -0.5)
