e5dae8bfb3375eb2ab5c85dc9ef16f9626cce52f
yulh <yulh@tuya.com>
2020-04-22 15:04:40 +0800

    add nlp training basic frame

---
+++ nlp_explore/train_frame/bframe.py
@@ -0,0 +1,168 @@
+#!/usr/bin/env python
+# coding=utf-8
+#================================================================
+#   Copyright (C) 2020 Fisher. All rights reserved.
+#   
+#   文件名称：train_frame.py
+#   创 建 者：YuLianghua
+#   创建日期：2020年04月19日
+#   描    述：NLP 训练通用框架
+#
+#================================================================
+
+import os
+import datetime
+import tensorflow as tf
+
+from data_process.data_processor import batch_iter 
+
+class TrainBaseFrame(object):
+    def __init__(self, model, flags, sess_config):
+        self.model = model
+        self.flags = flags
+        self.sess_config = sess_config
+        self.best_loss = float('inf')
+        self.global_step = tf.Variable(initial_value=0, trainable=False, name="global_step")
+
+    def init_summaries(self, sess):
+        ''' keep the track of gradient values and sparsity
+        '''
+        grad_summaries = []
+        grads_and_vars = self.model.grads_and_vars
+        for g,v in grads_and_vars:
+            if g is not None:
+                # gradient value
+                grad_hist_summary=tf.compat.v1.summary.histogram("{}/grad/hist".format(v.name),g)
+                grad_summaries.append(grad_hist_summary)
+                # sparity
+                sparsity_summary =tf.compat.v1.summary.scalar('{}/grad/sparsity'.format(v.name),tf.nn.zero_fraction(g))
+                grad_summaries.append(sparsity_summary)
+        grad_summaries_merged = tf.compat.v1.summary.merge(grad_summaries)
+
+        # summaries for loss and accuracy
+        loss_summary=tf.compat.v1.summary.scalar('loss', self.model.loss)
+        accuracy_summary=tf.compat.v1.summary.scalar('accuracy', self.model.acc)
+
+        # train summaries
+        self.train_summary_op = tf.compat.v1.summary.merge([loss_summary,accuracy_summary,grad_summaries_merged])
+        train_summary_dir=os.path.join(self.flags.out_dir,'summaries','train')
+        self.train_summary_writer=tf.compat.v1.summary.FileWriter(train_summary_dir,sess.graph)
+        # dev summaries
+        self.dev_summary_op = tf.compat.v1.summary.merge([loss_summary, accuracy_summary])
+        dev_summary_dir=os.path.join(self.flags.out_dir,'summaries','dev')
+        self.dev_summary_writer=tf.compat.v1.summary.FileWriter(dev_summary_dir,sess.graph)
+
+    def init_saver(self):
+        checkpoint_dir = os.path.abspath(os.path.join(self.flags.out_dir, 'checkpoint_dir'))
+        self.checkpoint_prefix = os.path.join(checkpoint_dir, "model")
+        if not os.path.exists(checkpoint_dir):
+            os.makedirs(checkpoint_dir)
+        saver = tf.train.Saver(tf.global_variables(), max_to_keep=self.flags.num_checkpoints)
+        return saver
+
+    def train_step(self, session, feed_dict):
+        if self.flags.summaries:
+            _, steps, summaries, loss, acc = session.run([self.train_op,
+                                                          self.global_step,
+                                                          self.train_summary_op,
+                                                          self.model.loss,
+                                                          self.model.acc], feed_dict=feed_dict)
+            nowtime=datetime.datetime.now().isoformat()
+            print('{}: step {}, loss {:g}, accuracy {:g}'.format(nowtime,steps,loss,acc))
+            self.train_summary_writer.add_summary(summaries,steps)
+        else:
+            _, steps, loss, acc = session.run([self.train_op,
+                                               self.global_step,
+                                               self.model.loss,
+                                               self.model.acc], feed_dict=feed_dict)
+            nowtime=datetime.datetime.now().isoformat()
+            print('{}: step {}, loss {:g}, accuracy {:g}'.format(nowtime,steps,loss,acc))
+
+    def eval_step(self, session, feed_dict):
+        if self.flags.summaries:
+            steps, summaries, loss, acc = session.run([self.global_step,
+                                                          self.dev_summary_op,
+                                                          self.model.loss,
+                                                          self.model.acc], feed_dict=feed_dict)
+            nowtime=datetime.datetime.now().isoformat()
+            print('{}: step {}, loss {:g}, accuracy {:g}'.format(nowtime,steps,loss,acc))
+            self.dev_summary_writer.add_summary(summaries,steps)
+        else:
+            steps, loss, acc = session.run([self.global_step,
+                                               self.model.loss,
+                                               self.model.acc], feed_dict=feed_dict)
+            nowtime=datetime.datetime.now().isoformat()
+            print('{}: step {}, loss {:g}, accuracy {:g}'.format(nowtime,steps,loss,acc))
+
+        return loss
+        
+    def early_stopping(self, current_loss):
+        if current_loss < self.best_loss:
+            self.best_loss = current_loss
+            self.last_improvement = 0
+        else:
+            self.last_improvement += 1
+
+        if self.last_improvement >= 20:
+            return True
+        else:
+            return False
+
+    def get_batches(self, train_data, batch_size, num_epochs, shuffle=True):
+        '''
+        if train_data: ([train_x], [train_y])
+        >>> data = list(zip(train_data))
+        >>> batches = batch_iter(data, batch_size, num_epochs, shuffle=shuffle)
+        >>> batches
+        '''
+        raise NotImplementedError
+
+    def get_feed_dict(self, batch_data, is_training=True):
+        '''
+        if batch_data is iteration like: (train_x, train_y)
+        >>> x_batch, y_batch = zip(*batch)
+        >>> feed_dict = {self.model.input_x:x_batch, self.model.input_y:y_batch, \
+        >>>                 self.model.dropout_keep_prob:self.flags.dropout_keep_prob}
+        '''
+        raise NotImplementedError
+
+    def train(self, sess, train_data, eval_data, test_data):
+        if self.flags.summaries:
+            self.init_summaries(sess)
+
+        self.train_op = self.model.optimizer.apply_gradients(
+                                            self.model.grads_and_vars,
+                                            global_step=self.global_step)
+
+        saver = self.init_saver()
+        sess.run(tf.global_variables_initializer())
+
+        batches = self.get_batches(train_data, self.flags.batch_size, 
+                                  self.flags.num_epochs)
+        for batch in batches:
+            feed_dict = self.get_feed_dict(batch, is_training=True)
+            self.train_step(sess, feed_dict)
+            current_step = tf.train.global_step(sess, self.global_step)
+
+            if current_step % self.flags.evaluate_every == 0:
+                feed_dict = self.get_feed_dict(eval_data)
+                print('\nEvaluation!')
+                current_loss = self.eval_step(sess, feed_dict)
+                print('\n')
+
+                # check early stopping
+                if self.early_stopping(current_loss):
+                    print("Early stopping is trigger at step: {} loss:{}".\
+                          format(current_step,current_loss))
+                    path = saver.save(sess, self.checkpoint_prefix, global_step=current_step)
+                    print("Saved model checkpoint to {}\n".format(path))
+                    break
+
+            if current_step % self.flags.checkpoint_every == 0:
+                path = saver.save(sess, self.checkpoint_prefix, global_step=current_step)
+                print("Saved model checkpoint to {}\n".format(path))
+
+        # finally test
+        feed_dict = self.get_feed_dict(test_data)
+        print('\nTest Finally!')
+        self.eval_step(sess, feed_dict)
---
+++ nlp_explore/train_frame/data_process/data_processor.py
@@ -0,0 +1,221 @@
+#!/usr/bin/env python
+# coding=utf-8
+#================================================================
+#   Copyright (C) 2020 Fisher. All rights reserved.
+#   
+#   文件名称：data_processor.py
+#   创 建 者：YuLianghua
+#   创建日期：2020年04月17日
+#   描    述：
+#
+#================================================================
+
+import pdb
+import numpy as np
+
+from collections import Counter
+from collections import defaultdict
+
+
+class DataProcessor(object):
+    def __init__(self, dpath, 
+                 ftype=1, 
+                 maxlen=100, 
+                 slabel=',',
+                 vpath=None,
+                 startline=0,
+                 padding=True,
+                 pvalue = 0,
+                 label_one_hot=True,
+                 s2_label=False,
+                 s2_label_dict = None
+                 ):
+        '''
+        @param dpath:
+        @param ftype: int,  1:`<s, y>`, 2:`<s, s>`, 3:`<s, s, y>`
+        @param maxlen: sequence max len; default 50;
+        @param slabel: split label; default `\t`;
+        @param vpath: vocab path;
+        @param startline:
+        @param padding: boolean, whether to padding; default true;
+        @param pvalue:
+        @param label_one_hot: boolean,
+        @param s2_label: boolean,
+        @param s2_label_dict: dict, label2id dict, like, {"B":0, "I":1, "O":2}
+        '''
+        self.dpath = dpath
+        self.ftype = ftype
+        self.maxlen = maxlen
+        self.slabel = slabel
+        self.vpath  = vpath
+        self.startline = startline
+        self.padding = padding
+        self.pvalue = pvalue
+        self.label_one_hot = label_one_hot
+        self.s2_label = s2_label
+        self.s2_label_dict = s2_label_dict
+        if not vpath:
+            self._init_vocab_dict()
+        else:
+            char_list = [c.strip() for c in open(self.vpath, 'r').readlines() if c]
+            self.char2idx = {c:idx for (idx, c) in enumerate(char_list)}
+            self.idx2char = {idx:c for (idx, c) in enumerate(char_list)}
+        self.data_list = defaultdict(list)
+
+    def _init_vocab_dict(self):
+        print("start to init vocab: %s" %self.dpath)
+        with open(self.dpath, 'r') as rf:
+            if self.ftype==1:
+                content = ''.join([l.strip().split(self.slabel)[0] for l in rf.readlines()])
+            elif self.ftype==2:
+                if s2_label:
+                    content = ''.join([l.strip().split(self.slabel)[0] for l in rf.readlines()])
+                else:
+                    content = ''.join([l.strip().replace('\t', '') for l in rf.readlines()])
+            elif self.ftype==3:
+                content = ''.join([l.strip().rsplit(self.label, 1)[0] for l in rf.readlines()])
+            char_count = Counter(content)
+            char_list = [c[0] for c in char_count.most_common(4000)]
+            char_list = ['<PAD>', '<UNK'] + char_list
+            self.char2idx = {c:idx for (c, idx) in enumerate(char_list)}
+            self.idx2char = {idx:c for (c, idx) in enumerate(char_list)}
+            # TODO
+            ## save char sets
+
+    def load_data(self):
+        '''load data
+        '''
+        if self.ftype==1:
+            fields = ['s1', 'label']
+        elif self.ftype==2:
+            fields = ['s1', 's2']
+        elif self.ftype==3:
+            fields = ['s1', 's2', 'label']
+        else:
+            raise ValueError()
+        with open(self.dpath, 'r') as rf:
+            for li, line in enumerate(rf):
+                if li<self.startline:
+                    continue
+                items = line.strip().split(self.slabel)
+                assert len(items)==len(fields)
+                for (f,seq) in zip(fields, items):
+                    self.data_list[f].append(seq)
+        self._doc2ids()
+
+    def _doc2ids(self):
+        self.doc2ids = dict()
+        if self.ftype==1 or self.ftype==3:
+            for f,seqs in self.data_list.items():
+                seq2ids = []
+                if f=="label":
+                    for seq in seqs:
+                        seq2ids.append(int(seq.strip()))
+                    if self.label_one_hot:
+                        a = np.array(seq2ids)
+                        b = np.zeros((a.size, a.max()+1))
+                        b[np.arange(a.size),a] = 1
+                        seq2ids = b
+                else:
+                    for seq in seqs:
+                        # <UNK> : 1 
+                        seq2id = [self.char2idx.get(c, 1) for c in seq]
+                        seq2id = padding(seq2id, maxlen=self.maxlen)
+                        seq2ids.append(np.array(seq2id))
+                self.doc2ids[f] = np.array(seq2ids)
+        elif self.ftype==2:
+            for f,seqs in self.data_list.items():
+                seq2ids = []
+                if f=="s2" and self.s2_label:
+                    for seq in seqs:
+                        seq2id = [self.s2_label_dict[seq.strip()] for c in seq]
+                        seq2id = padding(seq2id, maxlen=self.maxlen)
+                        seq2ids.append(np.array(seq2id))
+                else:
+                    for seq in seqs:
+                        # <UNK> : 1 
+                        seq2id = [self.char2idx.get(c, 1) for c in seq]
+                        seq2id = padding(seq2id, maxlen=self.maxlen)
+                        seq2ids.append(np.array(seq2id))
+                self.doc2ids[f] = np.array(seq2ids)
+
+        del self.data_list
+
+    def _split_index(self, x, eval_rate, test_rate):
+        train_rate = 1.0-(eval_rate+test_rate)
+        eval_sample_index = int(train_rate * float(len(x)))
+        test_sample_index= int((train_rate + eval_rate) * float(len(x)))
+        return eval_sample_index, test_sample_index
+
+    def data_split(self, shuffle=True, eval=0.1, test=0.1):
+        d = None
+        if self.ftype==1:
+            s = self.doc2ids['s1']
+            y = self.doc2ids['label']
+            if shuffle:
+                np.random.seed(10)
+                shuffle_indices = np.random.permutation(np.arange(len(y)))
+                s_shuffled = s[shuffle_indices]
+                y_shuffled = y[shuffle_indices]
+            s = s_shuffled if shuffle else s
+            y = y_shuffled if shuffle else y
+            d = (s,y)
+        elif self.ftype==2:
+            s = self.doc2ids['s1']
+            y = self.doc2ids['s2']
+            if shuffle:
+                np.random.seed(10)
+                shuffle_indices = np.random.permutation(np.arange(len(y)))
+                s_shuffled = s[shuffle_indices]
+                y_shuffled = y[shuffle_indices]
+            s = s_shuffled if shuffle else s
+            y = y_shuffled if shuffle else y
+            d = (s,y)
+        elif self.ftype==3:
+            s1 = self.doc2ids['s1']
+            s2 = self.doc2ids['s2']
+            y = self.doc2ids['label']
+            if shuffle:
+                np.random.seed(10)
+                shuffle_indices = np.random.permutation(np.arange(len(y)))
+                s1_shuffled = s1[shuffle_indices]
+                s2_shuffled = s2[shuffle_indices]
+                y_shuffled = y[shuffle_indices]
+            s1 = s1_shuffled if shuffle else s1
+            s2 = s2_shuffled if shuffle else s2
+            y = y_shuffled if shuffle else y
+            d = (s1, s2, y)
+        else:
+            raise ValueError()
+        self.split_data = defaultdict(list)
+        eval_split_index, test_split_index = self._split_index(d[-1], eval, test)
+        for l in d:
+            train, eval, test = l[:eval_split_index], \
+                                l[eval_split_index : test_split_index], \
+                                l[test_split_index:]
+            self.split_data['train'].append(train)
+            self.split_data['eval'].append(eval)
+            self.split_data['test'].append(test)
+            
+        return self.split_data
+
+def batch_iter(data, batch_size, epoch_num, shuffle=True):
+    '''generate a batch iterator for dataset;
+    '''
+    data = np.array(data)
+    data_size = len(data)
+    num_batches_per_epoch = int((data_size-1)/batch_size)+1
+    for epoch in range(epoch_num):
+        if shuffle:
+            shuffle_indices = np.random.permutation(np.arange(data_size))
+            shuffled_data = data[shuffle_indices]
+        else:
+            shuffled_data = data
+        for batch_num in range(num_batches_per_epoch):
+            start_index = batch_num*batch_size
+            end_index = min(batch_size*(batch_num+1), data_size)
+            yield shuffled_data[start_index:end_index]
+
+def padding(x, maxlen=50, pvalue=0):
+    x = x[:maxlen] if len(x)>=maxlen else x + [0]*(maxlen-len(x))
+    return x
