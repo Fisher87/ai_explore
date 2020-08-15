5c23f5b867e75e40434fe0ca2fc845e1e039504d
yulh <yulh@tuya.com>
2020-08-15 14:30:09 +0800

    update

--- nlp_explore/task/text_match/semantic/bert_match/task.py
+++ nlp_explore/task/text_match/semantic/bert_match/task.py
@@ -37,17 +37,17 @@ class TextMatchTask(object):
         "output_bias", [num_labels], initializer=tf.zeros_initializer())
 
     with tf.variable_scope("loss"):
-      if is_training:
+        if is_training:
         # I.e., 0.1 dropout
-        output_layer = tf.nn.dropout(output_layer, keep_prob=keep_prob)
+            output_layer = tf.nn.dropout(output_layer, keep_prob=keep_prob)
 
-      logits = tf.matmul(output_layer, output_weights, transpose_b=True)
-      self.logits = tf.nn.bias_add(logits, output_bias)
-      self.probabilities = tf.nn.softmax(logits, axis=-1)
-      log_probs = tf.nn.log_softmax(logits, axis=-1)
+    logits = tf.matmul(output_layer, output_weights, transpose_b=True)
+    self.logits = tf.nn.bias_add(logits, output_bias)
+    self.probabilities = tf.nn.softmax(logits, axis=-1)
+    log_probs = tf.nn.log_softmax(logits, axis=-1)
 
-      one_hot_labels = tf.one_hot(labels, depth=num_labels, dtype=tf.float32)
+    one_hot_labels = tf.one_hot(labels, depth=num_labels, dtype=tf.float32)
 
-      self.per_example_loss = -tf.reduce_sum(one_hot_labels * log_probs, axis=-1)
-      self.loss = tf.reduce_mean(self.per_example_loss)
+    self.per_example_loss = -tf.reduce_sum(one_hot_labels * log_probs, axis=-1)
+    self.loss = tf.reduce_mean(self.per_example_loss)
 
